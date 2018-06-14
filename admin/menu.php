740ff844ecbb0f380fdcc97072272455977325d2
Jonas Kuhn <jonaskuhn220@gmail.com>
2018-06-14 16:28:13 -0300

    dsv-IERCABC
    Criado admin

---
+++ admin/menu.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <a class="navbar-brand" href="?url=home.php">Admin</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Painel de Controle">
+                <a class="nav-link" href="?url=home.php">
+                    <i class="fa fa-fw fa-dashboard"></i>
+                    <span class="nav-link-text">Painel de Controle</span>
+                </a>
+            </li>
+            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Programação">
+                <a class="nav-link" href="?url=programacao.php">
+                    <i class="fa fa-fw fa-calendar"></i>
+                    <span class="nav-link-text">Programação</span>
+                </a>
+            </li>
+        </ul>
+        <ul class="navbar-nav sidenav-toggler">
+            <li class="nav-item">
+                <a class="nav-link text-center" id="sidenavToggler">
+                    <i class="fa fa-fw fa-angle-left"></i>
+                </a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="modal" data-target="#sairModal">
+                    <i class="fa fa-fw fa-sign-out"></i>Sair</a>
+            </li>
+        </ul>
+    </div>
+</nav>
