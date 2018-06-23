072a8ac4f8cbb7a98f714f7b41ea33a285c4a345
wellington <w3llingtonles@gmail.com>
2018-06-23 05:08:14 -0300

    # dsv-evento-IERCABC Desenvolvimento Web
    # Padrão de commit
    Programação
    
    Atualizado comandos e introdução de uma lista dinamica para remodelar com os eventos adicionei uma pasta com imagens para fins de testes que mais tarde sera removida e ajustada com as deevidas imagens e conteudos
    
    (Essa descrição é importante para o 'líder' ver o que cada um fez ou esta fazendo,
    portan

--- view/programacao.php
+++ view/programacao.php
@@ -1,43 +1,65 @@
 <?php
-//Menu 
-include './menu.php';
-//Cabecalho
 include './cabecalho.php';
-include '../conexao.php';
+include './menu.php';
 ?>
-<h1>Programação</h1>
-<?php
-$sql = "Select hora_inicio_prog, hora_fim_prog, obs_prog, img_prog, pavilhao_prog, video_prog "
-        . "from tb_programacao";
-$query = $pdo->query($sql);
-while ($dados = $query->fetch()) {
-    $hora_inicio_prog = $dados['hora_inicio_prog'];
-    $hora_fim_prog = $dados['hora_fim_prog'];
-    $obs_prog = $dados['obs_prog'];
-    $img_prog = $dados['img_prog'];
-    $pavilhao_prog = $dados['pavilhao_prog'];
-    $video_prog = $dados['video_prog'];
-    ?>
-    <?php
-}
-?>
-    
-<div class="container">
-    <div class="col-lg-8 col-md-10 mx-auto">
-        <ul>
-            <li>Alvorada Festiva  06:00 Horas.</li>
-            <li>Recepção 10:00 Horas </li>
-            <li>Pronunciamentos e Abertura do Evento 11:00Horas</li>
-            <li>Início do Almoço 11:30</li>              
-        </ul>
-        <H2>Atrações:</H2> 
-        <ul>
-            
-            <li>Banda KN início 10:30 às 12:30</li>
-            <li>Banda Festão 13:00 às 18:00</li>
-            <li>Banda Chopão </li>
-        </ul>
-    </div>
-</div>
-<?php
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" style="text\css" href=".css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+        <div id="portfolio" class="container-fluid text-center bg-grey">
+            <h2>Programação.</h2><br>
+            <h4>Venha e participar você também.</h4>
+            <div class="row text-center slideanim">
+                <div class="col-sm-4">
+                    <div class="thumbnail">
+                        <img src="img/asus zenfone go.jpg" alt="ASUS" width="400" height="300">
+                        <p>Smartphone Asus Zenfone Go ZB500KG-3H028BR, Quad Core, Android 5.1, Tela 5´, 8GB, 8MP, 3G, Dual Chip Desbloqueado - Prata</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="thumbnail">
+                        <img src="img/iphone7.jpg" alt="APPLE" width="400" height="300">
+                        <p>Apple iPhone 7, Chip A10, iOS 10, Tela 4,7´, 32GB, Câmera 12MP, 4G, Desbloqueado MN8X2/A Dourado</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="thumbnail">
+                        <img src="img/lgk10.jpg" alt="LG" width="400" height="300">
+                        <p>Smartphone LG K10 K430TV, Octa Core, Android 6.0, Tela 5.3´, 16GB, 13MP, 4G, Dual Chip, Desbloqueado - Indigo </p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="thumbnail">
+                        <img src="img/monitoracer.jpg" alt="LG" width="400" height="300">
+                        <p>Monitor Acer 21,5´ Full HD 5ms VGA/DVI Ajuste de Inclinação de -5 a 25 Graus VESA K222HQL</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="thumbnail">
+                        <img src="img/monitorlg.jpg" alt="LG" width="400" height="300">
+                        <p>Monitor LG 19.5´ com Ajuste de Altura DVI Black Piano 20M35PD-M</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="thumbnail">
+                        <img src="img/monitoraoc.jpg" alt="LG" width="400" height="300">
+                        <p>Monitor AOC LED 18.5´ HD Widescreen Ultra High DCR OSD VGA E970SWNL Preto</p>
+                    </div>
+                </div>
+            </div><br>
+            <footer class="container-fluid text-center">
+                <a href="#myPage" title="To Top">
+                    <span class="glyphicon glyphicon-chevron-up"></span>
+                </a>
+                <p>Voltar ao Início.</p>
+            </footer>
+    </body>
+</html>
