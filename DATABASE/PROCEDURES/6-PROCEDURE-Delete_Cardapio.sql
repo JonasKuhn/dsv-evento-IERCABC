653e4bfd958ff34ff8f56fdd1fa6a63a3990fb75
Jonas Kuhn <jonaskuhn220@gmail.com>
2018-06-25 16:37:39 -0300

    Revert "dsv-IERCABC"
    This reverts commit 3ee079efd8bdefa1155d47d14d53effdb3549be9.

--- DATABASE/PROCEDURES/6-PROCEDURE-Delete_Cardapio.sql
+++ DATABASE/PROCEDURES/6-PROCEDURE-Delete_Cardapio.sql
@@ -1,25 +1,5 @@
 DELIMITER $$
-CREATE PROCEDURE del_cardapio(x INT)
+CREATE PROCEDURE del_cardapio(cod INT)
 BEGIN
-	DECLARE aux_e INT;
-    DECLARE aux_ct INT;
-    
-    SET aux_e = 0;
-    SET aux_ct = 0;
-    
-    select COUNT(cod_evento) from tb_evento 
-    where cod_cardapio = x INTO aux_e;
-	
-    select COUNT(cod_cardapio_tipo) from tb_cardapio_tipo
-    where cod_cardapio = x INTO aux_ct;
-    
-    IF(aux_e != 0)THEN
-		IF(aux_ct != 0)THEN
-			SELECT 'Este cardápio esta sendo usado!';
-        ELSE
-			delete from tb_cardapio where cod_cardapio = cod;
-        END IF;
-    ELSE
-		SELECT 'Este cardápio esta sendo usado!';
-    END IF;
+	delete from tb_cardapio where cod_cardapio = cod;
 END;
--- DATABASE/PROCEDURES/7-PROCEDURE-Delete_Contato.sql
+++ DATABASE/PROCEDURES/7-PROCEDURE-Delete_Contato.sql
@@ -1,16 +1,7 @@
 DELIMITER $$
 CREATE PROCEDURE del_contato(x INT)
 BEGIN
-	DECLARE aux_ec INT;
-	SET aux_ec = 0;
-    
-    select COUNT(cod_evento_contato) from tb_evento_contato
-    where cod_contato = x INTO aux_ec;
-    
-    IF(aux_ec != 0)THEN
-		SELECT 'Contato esta sendo usado por um Evento!';
-	ELSE
-		DELETE FROM tb_evento_contato WHERE cod_contato = x;
-		DELETE FROM tb_contato WHERE cod_contato = x;
-    END IF;
+    DELETE FROM tb_evento_contato WHERE cod_contato = x;
+
+	DELETE FROM tb_contato WHERE cod_contato = x;
 END$$;
