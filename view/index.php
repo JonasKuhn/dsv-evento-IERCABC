3bdef8e1363787fa13f61ae2730221dec1a180e3
Pedro Henrique Moro de Lima <pedrohmorolima@hotmail.com>
2018-06-26 21:46:39 -0300

    dsv-IERCABC

--- view/index.php
+++ view/index.php
@@ -50,7 +50,7 @@
             case 'postagem': include ('./postagem.php');
                 break;
             default : include ('./inicio.php');
-        }   
+        }
         ?>
 
         <!-- Bootstrap core JavaScript -->
@@ -58,71 +58,69 @@
         <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
         <!-- Custom scripts for this template -->
         <script src="../js/clean-blog.min.js"></script>
-        
         <div class="rodape">
-            <footer>
-        <div class="container_rodape">
-            <div>
-                <h3>Pontos de venda</h3>
-                <ul class="list-style-type"><h5>São João do Oeste</h5>
-                    <li><a href='https://www.facebook.com/Agropecuaria.05'>Agropecuária Bressler</a></li>
-                    <li>Fone:</li>
-                </ul>
-                <ul class="list-style-type"><h5>Itapiranga</h5>
-                    <li><a href="https://www.facebook.com/sorveteriatropicalfw/">Sorveteria Tropical</a></li>
-                    <li>Fone:</li>
-                </ul>
-                <ul class="list-style-type"><h5>Sede Capela</h5>
-                    <li><a href="https://www.facebook.com/pages/Bar-e-Lanchonete-Capelense/193612621463936">Bar e Lanchonete Capelense</a></li>
-                    <li>Fone:</li>
-                </ul>
-                <ul class="list-style-type"><h5>Tunápolis</h5>
-                    <li><a href='https://www.facebook.com/Restaurante-Pauli-Ltda-660073460750774/'>Restaurante Pauli Ltda</a></li>
-                    <li>Fone:</li>
-                </ul>
-                <ul class="list-style-type"><h5>Iporã do Oeste</h5>
-                    <li><a href="https://www.facebook.com/hcerta/">Hora Certa Conveniência e Cervejaria<a></li>
-                                <li>Fone:</li>
-                                </ul> 
-                                </div>
-                                </div>
-                                <div class="mapa">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1497.5973737223667!2d-53.63836532479368!3d-27.141147294873765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94fbb13ab8dfaee7%3A0x86fb4a55813d5cbe!2sUnnamed+Road%2C+S%C3%A3o+Jo%C3%A3o+do+Oeste+-+SC%2C+89897-000!5e1!3m2!1spt-BR!2sbr!4v1529433693442" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-                                 </div>
-                                <div class="trava"></div>
-                                <div class="row">
-                                    <div class="col-lg-8 col-md-10 mx-auto">
-                                        <ul class="list-inline text-center">
-                                            <li class="list-inline-item">
-                                                <a href="#">
-                                                    <span class="fa-stack fa-lg">
-                                                        <i class="fa fa-circle fa-stack-2x"></i>
-                                                        <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                                                    </span>
-                                                </a>
-                                            </li>
-                                            <li class="list-inline-item">
-                                                <a href="#">
-                                                    <span class="fa-stack fa-lg">
-                                                        <i class="fa fa-circle fa-stack-2x"></i>
-                                                        <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                                                    </span>
-                                                </a>
-                                            </li>
-                                            <li class="list-inline-item">
-                                                <a href="#">
-                                                    <span class="fa-stack fa-lg">
-                                                        <i class="fa fa-circle fa-stack-2x"></i>
-                                                        <i class="fa fa-github fa-stack-1x fa-inverse"></i>
-                                                    </span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                        <p class="copyright text-muted">Copyright &copy; Your Website 2018</p>
-                                    </div>
-                                </div>
-                                
-            </footer>
+        <footer>
+            <div class="col-12">
+                <div class="col-6 float-left">
+                    <h5>Pontos de venda</h5>
+                    <ul class="list-style-type"><h6>São João do Oeste</h6>
+                        <li><a href='https://www.facebook.com/Agropecuaria.05'>Agropecuária Bressler</a></li>
+                        <li>Fone:</li>
+                    </ul>
+                    <ul class="list-style-type"><h6>Itapiranga</h6>
+                        <li><a href="https://www.facebook.com/sorveteriatropicalfw/">Sorveteria Tropical</a></li>
+                        <li>Fone:</li>
+                    </ul>
+                    <ul class="list-style-type"><h6>Sede Capela</h6>
+                        <li><a href="https://www.facebook.com/pages/Bar-e-Lanchonete-Capelense/193612621463936">Bar e Lanchonete Capelense</a></li>
+                        <li>Fone:</li>
+                    </ul>
+                    <ul class="list-style-type"><h6>Tunápolis</h6>
+                        <li><a href='https://www.facebook.com/Restaurante-Pauli-Ltda-660073460750774/'>Restaurante Pauli Ltda</a></li>
+                        <li>Fone:</li>
+                    </ul>
+                    <ul class="list-style-type"><h6>Iporã do Oeste</h6>
+                        <li><a href="https://www.facebook.com/hcerta/">Hora Certa Conveniência e Cervejaria</a></li>
+                        <li>Fone:</li>
+                    </ul> 
+                </div>
+                <div class="mapa col-6 float-right">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1497.5973737223667!2d-53.63836532479368!3d-27.141147294873765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94fbb13ab8dfaee7%3A0x86fb4a55813d5cbe!2sUnnamed+Road%2C+S%C3%A3o+Jo%C3%A3o+do+Oeste+-+SC%2C+89897-000!5e1!3m2!1spt-BR!2sbr!4v1529433693442" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="trava"></div>
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                    <ul class="list-inline text-center">
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted">Copyright &copy; Your Website 2018</p>
+                </div>
+            </div>
+        </footer>
         </div>
     </body>
 </html>
